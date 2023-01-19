039ad6eef691f4d389268417076013fa5d79e9b1
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-01-18 21:54:34 -0300

    [fixing variable name] comlpetedPercentage -> completedPercentage

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -9,18 +9,18 @@ interface HabitDayProps {
 
 export function HabitDay({ completed, amount }: HabitDayProps) {
 
-  const comlpetedPercentage =  Math.round((completed / amount) * 100);
+  const completedPercentage =  Math.round((completed / amount) * 100);
 
   return (
     <Popover.Root>
       <Popover.Trigger
         className={clsx("w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg", {
-          'bg-zinc-900 border-zinc-800': comlpetedPercentage === 0,
-          'bg-violet-900 border-violet-500': comlpetedPercentage > 0 && comlpetedPercentage < 20,
-          'bg-violet-800 border-violet-500': comlpetedPercentage >= 20 && comlpetedPercentage < 40,
-          'bg-violet-700 border-violet-500': comlpetedPercentage >= 40 && comlpetedPercentage < 60,
-          'bg-violet-600 border-violet-500': comlpetedPercentage >= 60 && comlpetedPercentage < 80,
-          'bg-violet-500 border-violet-400': comlpetedPercentage >= 80,
+          'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+          'bg-violet-900 border-violet-500': completedPercentage > 0 && completedPercentage < 20,
+          'bg-violet-800 border-violet-500': completedPercentage >= 20 && completedPercentage < 40,
+          'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
+          'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+          'bg-violet-500 border-violet-400': completedPercentage >= 80,
         })}
       />
 
@@ -29,7 +29,7 @@ export function HabitDay({ completed, amount }: HabitDayProps) {
           <span className="font-semibold text-zinc-400">terça-feira</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">17/01</span>
 
-          <ProgressBar progress={comlpetedPercentage} />
+          <ProgressBar progress={completedPercentage} />
 
           <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
         </Popover.Content>
