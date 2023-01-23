ce442ca9e78d1a71ce1909129ea47777505c9036
Thiago José de Queiroz <42189768+sauba@users.noreply.github.com>
2023-01-23 01:58:42 -0300

    Update wrong typed variables at HabitDay.tsx
    After take a fast look a this code  trying to fix some of mine code errors. i have found some wrong typed variables and have made some suggestions. hope it helps.

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -5,21 +5,21 @@ import dayjs from 'dayjs';
 import { HabitsList } from './HabitsList';
 import { useState } from 'react';
 
-interface HabitDayProps {
+interface IHabitDayProps {
   date: Date
   defaultCompleted?: number
   amount?: number
 }
 
-export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayProps) {
+export function HabitDay({ defaultCompleted = 0, amount = 0, date }: IHabitDayProps) {
   const [completed, setCompleted] = useState(defaultCompleted)
 
-  const comlpetedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0;
+  const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0;
 
   const dayAndMonth = dayjs(date).format('DD/MM')
   const dayOfWeek = dayjs(date).format('dddd')
 
-  function handleCompletedChaged(completed: number) {
+  function handleCompletedChanged(completed: number) {
     setCompleted(completed)
   }
 
@@ -27,12 +27,12 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
     <Popover.Root>
       <Popover.Trigger
         className={clsx("w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background", {
-          'bg-zinc-900 border-zinc-800': comlpetedPercentage === 0,
-          'bg-violet-900 border-violet-500': comlpetedPercentage > 0 && comlpetedPercentage < 20,
-          'bg-violet-800 border-violet-500': comlpetedPercentage >= 20 && comlpetedPercentage < 40,
-          'bg-violet-700 border-violet-500': comlpetedPercentage >= 40 && comlpetedPercentage < 60,
-          'bg-violet-600 border-violet-500': comlpetedPercentage >= 60 && comlpetedPercentage < 80,
-          'bg-violet-500 border-violet-400': comlpetedPercentage >= 80,
+          'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+          'bg-violet-900 border-violet-500': completedPercentage > 0 && completedPercentage < 20,
+          'bg-violet-800 border-violet-500': completedPercentage >= 20 && completedPercentage < 40,
+          'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
+          'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+          'bg-violet-500 border-violet-400': completedPercentage >= 80,
         })}
       />
 
@@ -41,9 +41,9 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
           <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
 
-          <ProgressBar progress={comlpetedPercentage} />
+          <ProgressBar progress={completedPercentage} />
 
-          <HabitsList date={date} onCompletedChanged={handleCompletedChaged} />
+          <HabitsList date={date} onCompletedChanged={handleCompletedChanged} />
 
           <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
         </Popover.Content>
