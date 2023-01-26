24f380a88cf69d4f682271aa1a0d1f4252543b83
rodrigo <rodrigo.rgtic@gmail.com>
2023-01-26 20:19:36 -0300

    feat: local notification

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,18 @@
 import './src/lib/dayjs';
 
 import { StatusBar } from 'react-native';
-import { 
+import {
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter';
+import * as Notifications from 'expo-notifications';
 
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
+import { useEffect } from 'react';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,6 +21,31 @@ export default function App() {
     Inter_700Bold,
     Inter_800ExtraBold
   });
+
+  async function schedulePushNotification() {
+    const schedule = await Notifications.getAllScheduledNotificationsAsync();
+    console.log("Agendadas: ", schedule);
+
+    if (schedule.length > 0) {
+      await Notifications.cancelAllScheduledNotificationsAsync();
+    }
+
+    const trigger = new Date(Date.now());
+    trigger.setHours(trigger.getHours() + 5);
+    trigger.setSeconds(0);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Olá, Rodrigo! 😀",
+        body: "Você praticou seus hábitos hoje?"
+      },
+      trigger
+    });
+  }
+
+  useEffect(() => {
+    schedulePushNotification();
+  }, []);
 
   if (!fontsLoaded) {
     return (
